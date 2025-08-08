84b5007be1e977f2aad7a04336a0d90015209b81
juheeNoh123 <juhee10131013@gmail.com>
2025-08-08 22:51:50 +0900

    Initial commit

---
+++ src/main/java/tomato/movingcash/MovingcashApplication.java
@@ -0,0 +1,13 @@
+package tomato.movingcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingcashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovingcashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tomato/movingcash/MovingcashApplicationTests.java
@@ -0,0 +1,13 @@
+package tomato.movingcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovingcashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
